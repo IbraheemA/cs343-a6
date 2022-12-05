b0543c8ad2c7d2bcd1f183ade714c21bef56dc44
Vaenthan Jeevarajah <v2jeevarajah@uwaterloo.ca>
2022-12-05 15:27:45 -0500

    fix printer

--- printer.h
+++ printer.h
@@ -8,13 +8,39 @@ _Monitor Printer {
 	unsigned int numVendingMachines;
 	unsigned int numCouriers;
 
-	std::string *printBuffer;
+	class PrintEntry {
+		char state;
+		unsigned int value1;
+		unsigned int value2;
+		unsigned int numArgs;
+
+		public:
+			PrintEntry( char state) : state{state}, numArgs{1} {}
+			PrintEntry( char state, unsigned int value1 ) : state{state}, value1{value1}, numArgs{2} {}
+			PrintEntry( char state, unsigned int value1, unsigned int value2 ) : state{state}, value1{value1}, value2{value2}, numArgs{3} {}
+
+			void print() {
+				std::cout << state;
+				if (numArgs > 1) {
+					std::cout << value1;
+				}
+				if (numArgs > 2) {
+					std::cout << "," << value2;
+				}
+			}
+	};
+
+	PrintEntry **printBuffer;
 
 	void flushBuffer(int index) {
-		if (printBuffer[index] != "") {
+		if (printBuffer[index] != nullptr) {
 			for (int i = 0; i < 6 + numStudents + numVendingMachines + numCouriers; i++) {
-				std::cout << printBuffer[i] << "\t";
-				printBuffer[i] = "";
+				if (printBuffer[i] != nullptr) {
+					printBuffer[i]->print();
+					std::cout << "\t";
+					delete printBuffer[i];
+					printBuffer[i] = nullptr;
+				}
 			}
 			std::cout << std::endl;
 		}
@@ -41,40 +67,47 @@ _Monitor Printer {
 	enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
 	Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers ) :
 		numStudents{numStudents}, numVendingMachines{numVendingMachines}, numCouriers{numCouriers} {
-			printBuffer = new std::string[6 + numStudents + numVendingMachines + numCouriers];
+			printBuffer = new PrintEntry*[6 + numStudents + numVendingMachines + numCouriers];
 
 			for (int i = 0; i < 6 + numStudents + numVendingMachines + numCouriers; i++) {
-				printBuffer[i] = "";
+				printBuffer[i] = nullptr;
 			}
 		}
 	~Printer() {
-		delete printBuffer;
+		for (int i = 0; i < 6 + numStudents + numVendingMachines + numCouriers; i++) {
+			if (printBuffer[i] != nullptr) {
+				delete printBuffer[i];
+			}
+		}
+
+		delete [] printBuffer;
 	}
+
 	void print( Kind kind, char state ) {
 		flushBuffer(kind);
-		printBuffer[kind] = state;
+		printBuffer[kind] = new PrintEntry(state);
 	}
 	void print( Kind kind, char state, unsigned int value1 ) {
 		flushBuffer(kind);
-		// printBuffer[kind] = state + value1;
+		printBuffer[kind] = new PrintEntry(state, value1);
 	}
 	void print( Kind kind, char state, unsigned int value1, unsigned int value2 ) {
 		flushBuffer(kind);
-		// printBuffer[kind] = std::string(state) + " " + std::string(value1) + "," + std::string(value2);
+		printBuffer[kind] = new PrintEntry(state, value1, value2);
 	}
 	void print( Kind kind, unsigned int lid, char state ) {
 		int index = getIndex(kind, lid);
 		flushBuffer(index);
-		// printBuffer[index] = state;
+		printBuffer[index] = new PrintEntry(state);
 	}
 	void print( Kind kind, unsigned int lid, char state, unsigned int value1 ) {
 		int index = getIndex(kind, lid);
 		flushBuffer(index);
-		// printBuffer[index] = state + value1;
+		printBuffer[index] = new PrintEntry(state, value1);
 	}
 	void print( Kind kind, unsigned int lid, char state, unsigned int value1, unsigned int value2 ) {
 		int index = getIndex(kind, lid);
 		flushBuffer(index);
-		// printBuffer[index] = std::string(state) + " " + std::string(value1) + "," + std::string(value2);
+		printBuffer[index] = new PrintEntry(state, value1, value2);
 	}
 };
