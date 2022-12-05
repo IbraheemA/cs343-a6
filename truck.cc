e83a9e88ef8cc6256fe21f9cc9ee98441720ba20
Vaenthan Jeevarajah <v2jeevarajah@uwaterloo.ca>
2022-12-05 14:54:15 -0500

    truck

--- truck.cc
+++ truck.cc
@@ -1,30 +1,39 @@
-#pragma once
+#include "truck.h"
 
-_Task Truck {
-    Printer & prt;
-    NameServer & nameServer;
-    BottlingPlant & plant;
-    unsigned int numVendingMachines;
-    unsigned int maxStockPerFlavour;
+void Truck::main() {
+  try {
+    VendingMachine ** vms = nameServer.getMachineList();
+    for (;;) {
+      yield(prng(1, 10));
+      plant.getShipment(cargo);
 
-    unsigned int * cargo;
-
-    int lastRestocked;
-
-    void main() {
-      VendingMachine ** vms = nameServer.getMachineList();
-      for (;;) {
-        yield(prng(1, 10));
-        plant.getShipment(cargo);      
-      }
-    }
-  public:
-    Truck( Printer & prt, NameServer & nameServer, BottlingPlant & plant,
-        unsigned int numVendingMachines, unsigned int maxStockPerFlavour ) :
-        prt{prt}, nameServer{nameServer}, plant{plant},
-        numVendingMachines{numVendingMachines}, maxStockPerFlavour{maxStockPerFlavour} {
-          cargo = new int[numVendingMachines];
-          lastRestocked = 0;
+      for (int i = 0; i < numVendingMachines; i++) {
+        unsigned int inventory[NUM_OF_FLAVOURS] = vms[restockIndex].inventory();
+        int inventoryCount = 0;
+        for (int j = 0; j < NUM_OF_FLAVOURS; j++) {
+          if (cargo[j] > 0 && inventory[j] < maxStockPerFlavour) {
+            int newInventory = max(inventory[j] + cargo[j], maxStockPerFlavour);
+            cargo[j] = inventory[j] + cargo[j] - newInventory;
+            inventory[j] = newInventory;
+            inventoryCount += cargo[j];
+          }
         }
+        if (prng(100) == 0) {
+          yield(10);
+        }
+        vms[restockIndex++].restocked();      
+        if (inventoryCount == 0) {
+          break;
+        }
+      }    
+    }
+  } catch(Shutdown e) {}
+}
 
-};
+Truck::Truck( Printer & prt, NameServer & nameServer, BottlingPlant & plant,
+    unsigned int numVendingMachines, unsigned int maxStockPerFlavour ) :
+    prt{prt}, nameServer{nameServer}, plant{plant},
+  numVendingMachines{numVendingMachines}, maxStockPerFlavour{maxStockPerFlavour} {
+    cargo = new int[numVendingMachines];
+    restockIndex = 0;
+}
--- truck.h
+++ truck.h
@@ -1,6 +1,16 @@
 #pragma once
 
 _Task Truck {
+    Printer & prt;
+    NameServer & nameServer;
+    BottlingPlant & plant;
+    unsigned int numVendingMachines;
+    unsigned int maxStockPerFlavour;
+
+    unsigned int * cargo;
+
+    int restockIndex;
+
     void main();
   public:
     Truck( Printer & prt, NameServer & nameServer, BottlingPlant & plant,
