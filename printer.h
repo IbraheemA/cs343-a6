c6bf3cd4ca64e801e85bcb8643ba8fa9863117dd
Vaenthan Jeevarajah <v2jeevarajah@uwaterloo.ca>
2022-12-04 22:04:49 -0500

    fixing printer

--- printer.h
+++ printer.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include <string>
+#include <string> 
+#include <iostream>
 
 _Monitor Printer {
 	unsigned int numStudents;
@@ -11,15 +12,15 @@ _Monitor Printer {
 
 	void flushBuffer(int index) {
 		if (printBuffer[index] != "") {
-			for (std::string s: printBuffer) {
-				std::cout << s << "\t";
-				s = "";
+			for (int i = 0; i < 6 + numStudents + numVendingMachines + numCouriers; i++) {
+				std::cout << printBuffer[i] << "\t";
+				printBuffer[i] = "";
 			}
 			std::cout << std::endl;
 		}
 	}
 
-	int getIndex(Kind kind, unsigned int lid) {
+	int getIndex(int kind, unsigned int lid) {
 		int offset = 6 + lid;
 		switch (kind)
 		{
@@ -34,15 +35,16 @@ _Monitor Printer {
 		default:
 			break;
 		}
+		return offset;
 	}
   public:
 	enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
 	Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers ) :
 		numStudents{numStudents}, numVendingMachines{numVendingMachines}, numCouriers{numCouriers} {
-			printBuffer = new string[6 + numStudents + numVendingMachines + numCouriers];
+			printBuffer = new std::string[6 + numStudents + numVendingMachines + numCouriers];
 
-			for (string s: printBuffer) {
-				s = "";
+			for (int i = 0; i < 6 + numStudents + numVendingMachines + numCouriers; i++) {
+				printBuffer[i] = "";
 			}
 		}
 	~Printer() {
@@ -61,17 +63,17 @@ _Monitor Printer {
 		printBuffer[kind] = state + " " + value1 + "," + value2;
 	}
 	void print( Kind kind, unsigned int lid, char state ) {
-		int index = getIndex(kind, lid)
+		int index = getIndex(kind, lid);
 		flushBuffer(index);
 		printBuffer[index] = state;
 	}
 	void print( Kind kind, unsigned int lid, char state, unsigned int value1 ) {
-		int index = getIndex(kind, lid)
+		int index = getIndex(kind, lid);
 		flushBuffer(index);
 		printBuffer[index] = state + value1;
 	}
 	void print( Kind kind, unsigned int lid, char state, unsigned int value1, unsigned int value2 ) {
-		int index = getIndex(kind, lid)
+		int index = getIndex(kind, lid);
 		flushBuffer(index);
 		printBuffer[index] = state + " " + value1 + "," + value2;
 	}
--- truck.cc
+++ truck.cc
@@ -7,11 +7,24 @@ _Task Truck {
     unsigned int numVendingMachines;
     unsigned int maxStockPerFlavour;
 
-    void main();
+    unsigned int * cargo;
+
+    int lastRestocked;
+
+    void main() {
+      VendingMachine ** vms = nameServer.getMachineList();
+      for (;;) {
+        yield(prng(1, 10));
+        plant.getShipment(cargo);      
+      }
+    }
   public:
     Truck( Printer & prt, NameServer & nameServer, BottlingPlant & plant,
         unsigned int numVendingMachines, unsigned int maxStockPerFlavour ) :
         prt{prt}, nameServer{nameServer}, plant{plant},
-        numVendingMachines{numVendingMachines}, maxStockPerFlavour{maxStockPerFlavour} {}
+        numVendingMachines{numVendingMachines}, maxStockPerFlavour{maxStockPerFlavour} {
+          cargo = new int[numVendingMachines];
+          lastRestocked = 0;
+        }
 
 };
